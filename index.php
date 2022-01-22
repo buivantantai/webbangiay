4d5e4967f84737f43d48023bf95ab1969dcd3e32
buivantantai <buivantantai112@gmail.com>
2022-01-22 07:45:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web bán giày</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header class="sticky-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-2 menu" id="logo">
+					<img src="img/logo1.jpg">
+				</div>
+				<div class="col-2">
+					
+				</div>
+				<div class="col-8 menu">
+					<ul>
+						<li><a href="">Trang chủ</a></li>
+						<li><a href="">Sản phẩm</a>
+							<ul class="menu_child">
+								<li><a href="#Adidas">Adidas</a></li>
+								<li><a href="#Nike">Nike</a></li>
+								<li><a href="#Vans">Vans</a></li>
+							</ul>
+						</li>
+						<li class="shop"><a href="">Giỏ hàng</a></li>
+						<li><a href="">Đăng nhập</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		<script>
+			$('.menu_child').parent('li').addClass('has-child');
+		</script>
+	</header>
+	
+	<!-- Slideshow container -->
+	<div class="slideshow-container">
+
+		<div class="mySlides fade">
+		  <div class="numbertext">1 / 3</div>
+		  <img src="img/slide/slide1.jpg" style="width:100%">
+		</div>
+
+		<div class="mySlides fade">
+		  <div class="numbertext">2 / 3</div>
+		  <img src="img/slide/slide2.jpg" style="width:100%">
+		</div>
+
+		<div class="mySlides fade">
+		  <div class="numbertext">3 / 3</div>
+		  <img src="img/slide/slide3.jpg" style="width:100%">
+		</div>
+	</div>
+	<br>
+	<div class="panel">
+		<div id="Adidas" class="panel-heading">
+			<h1>ADIDAS</h1>
+		</div>
+	<div class="qcc">
+		<div class="borderr">
+			<div class="panel-body">
+				<div class="sanpham">
+				<div class="sanpham-1 re-padding">
+					<div class="sanpham-item">
+						<div class="sanpham-name">
+								<a href="" style="color: #337ab7">Adidas Originals White</a>
+						</div>
+						<div class="sanpham-image">
+							<img src="img/product/Giay_Stan_Smith_trang_M20325_01_standard.jpg">
+						</div>
+						<div>
+							<p class="gia">Giá: <strong style="color: red;">2.300.000 VNĐ</strong></span></p>
+						</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-2 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">ADIDAS STAN SMITH PRIMEBLUE</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/Giay_Stan_Smith_Primeblue_trang_FX5688_01_standard.jpg">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">2.000.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-3 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Adidas Originals SUPERSTAR</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/Giay_Superstar_trang_EG4958_01_standard.jpg">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">1.369.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-4 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">SUPERSTAR SIMPSONS SQUISHEE</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/SUPERSTAR_SIMPSONS_SQUISHEE_mau_xanh_la_H05789_01_standard.jpg">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">2.400.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="panel">
+		<div id="Nike" class="panel-heading">
+			<h1>Nike</h1>
+		</div>
+	<div class="qcc">
+		<div class="borderr">
+			<div class="panel-body">
+				<div class="sanpham">
+				<div class="sanpham-1 re-padding">
+					<div class="sanpham-item">
+						<div class="sanpham-name">
+								<a href="" style="color: #337ab7">Nike Air Force 1 '07</a>
+						</div>
+						<div class="sanpham-image">
+							<img src="img/product/air-force-1-07-shoe-AKTdww3y.png">
+						</div>
+						<div>
+							<p class="gia">Giá: <strong style="color: red;">2.500.000 VNĐ</strong></span></p>
+						</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-2 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Nike Air Force 1 Shadow</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/air-force-1-shadow-shoe-klCJXd.png">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">3.239.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-3 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Nike Air Force 1 High '07 EMB</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/air-force-1-high-07-emb-shoe-NZvJXr.png">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">3.369.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-4 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Nike Air Max Genome</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/air-max-genome-shoe-lQ1nNw.png">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">4.400.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="panel">
+		<div id="Vans" class="panel-heading">
+			<h1>Vans</h1>
+		</div>
+	<div class="qcc">
+		<div class="borderr">
+			<div class="panel-body">
+				<div class="sanpham">
+				<div class="sanpham-1 re-padding">
+					<div class="sanpham-item">
+						<div class="sanpham-name">
+								<a href="" style="color: #337ab7">Vans Slip-On Flame</a>
+						</div>
+						<div class="sanpham-image">
+							<img src="img/product/vn0a38f7phn-5.jpg">
+						</div>
+						<div>
+							<p class="gia">Giá: <strong style="color: red;">1.300.000 VNĐ</strong></span></p>
+						</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-2 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Vans Slip-On Tiger Patchwork</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/vn0a4u381io-6.jpg">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">1.450.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-3 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Vans Slip-On Anaheim</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/vn0a3jexpu1-11.png">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">1.550.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+					<div class="sanpham-4 re-padding">
+						<div class="sanpham-item">
+							<div class="sanpham-name">
+									<a href="" style="color: #337ab7">Vans Old Skool Skate</a>
+								</div>
+							<div class="sanpham-image">
+								<img src="img/product/vn0a5fcby28-4.jpg">
+							</div>
+							<div>
+								<p class="gia">Giá: <strong style="color: red;">2.200.000 VNĐ</strong></span></p>
+							</div>
+							<a href="">
+								<button class="btn btn-info"><span>Thêm vào giỏ hàng</span></button>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="ctbh">
+		<div class="container">
+			<div class="row">
+				<div>
+					<ul class="menu_footer">
+						<li><h4>VỀ CHÚNG TÔI</h2></li>
+						<li><p href="">Giới thiệu về shop</p></li>
+						<li><p href="">Qui chế hoạt động</p></li>
+						<li><p href="">Điều khoản dịch vụ</p></li>
+					</ul>
+				</div>
+				<div>
+					<ul class="menu_footer">
+						<li><h4>CHÍNH SÁCH</h2></li>
+						<li><p href="">Chính sách giao hàng</p></li>
+						<li><p href="">Chính sách đổi trả</p></li>
+						<li><p href="">Chính sách bảo mật</p></li>
+					</ul>
+				</div>
+				<div>
+					<ul class="menu_footer">
+						<li><h4>HỖ TRỢ KHÁCH HÀNG</h2></li>
+						<li><p href="">Bảo vệ người mua</p></li>
+						<li><p href="">Quy định đối với người mua</p></li>
+						<li><p href="">Hướng dẫn mua hàng Online</p></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</body>
+	<script>
+	var slideIndex = 0;
+	showSlides();
+
+	function showSlides() {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("dot");
+	  for (i = 0; i < slides.length; i++) {
+	    slides[i].style.display = "none";  
+	  }
+	  slideIndex++;
+	  if (slideIndex > slides.length) {slideIndex = 1}    
+	  for (i = 0; i < dots.length; i++) {
+	    dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";  
+	  dots[slideIndex-1].className += " active";
+	  setTimeout(showSlides, 2000); // Change image every 2 seconds
+	}
+</script>
+
+</html>
